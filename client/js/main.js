dd7e2a3777250795f61f08523295a38e0db8224a
Kevin Schwarz <kschwarz99@gmail.com>
2021-10-19 20:14:35 -0400

    burger menu animation

--- client/js/main.js
+++ client/js/main.js
@@ -1,5 +1,5 @@
 function toggleMobileMenu() {
-  document.querySelector(".mobile-nav-menu").classList.toggle("hidden");
+  document.querySelector(".mobile-nav-menu").classList.toggle("is-active");
 }
 
 document
@@ -9,9 +9,3 @@ document
   .querySelector(".mobile-nav-menu__close-btn")
   .addEventListener("click", () => toggleMobileMenu());
 console.log(document.querySelector("section"));
-
-const burger_menu = document.querySelector(".burger-menu");
-
-burger_menu.addEventListener("click", function () {
-  burger_menu.classList.toggle("is-active");
-});
