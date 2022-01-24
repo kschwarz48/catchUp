ba3c33b6ccb38cca6253d2578111e480fd8fe650
Kevin J. Schwarz <35590084+kschwarz48@users.noreply.github.com>
2022-01-24 18:22:17 -0500

    login page to ReactJS

--- client/components/LoginPage/LoginPage.jsx
+++ client/components/LoginPage/LoginPage.jsx
@@ -1,7 +1,42 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./loginPage.css";
 
 const LoginPage = () => {
-  return <div>Login Page</div>;
+  return (
+    <main className="login__main">
+      <div className="login__container">
+        <h1 className="header__text">Welcome back!</h1>
+        <p className="header__subtext">
+          We're happy to see you're catching up!
+        </p>
+        <form action="/login" method="post">
+          <h2 className="login__label">email</h2>
+          <input className="login__input" type="email" name="email" required />
+          <h2 className="login__label">password</h2>
+          <input
+            className="login__input"
+            type="password"
+            name="password"
+            required
+          />
+          <Link className="login__link forgot" to="/forgot">
+            Forgot Password?
+          </Link>
+          <button className="login__button" type="submit">
+            Login
+          </button>
+        </form>
+
+        <p className="register">
+          Need Account?
+          <Link className="login__link register__button" to="/register">
+            Register
+          </Link>
+        </p>
+      </div>
+    </main>
+  );
 };
 
 export default LoginPage;
