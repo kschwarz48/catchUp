042408585bd93497ef045a2bf7e6276ad3083d15
Kevin J. Schwarz <35590084+kschwarz48@users.noreply.github.com>
2022-01-31 18:19:49 -0500

    fix reset.css

--- client/main.jsx
+++ client/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./components/App/App";
+import "./css/reset.css";
 
 ReactDOM.render(
   <React.StrictMode>
