eda93353febb6a4dddb92aa69d21e0063e88298f
Kevin J. Schwarz <35590084+kschwarz48@users.noreply.github.com>
2022-01-31 22:46:51 -0500

    login/register auth routes and axios init

--- client/components/LoginPage/LoginPage.jsx
+++ client/components/LoginPage/LoginPage.jsx
@@ -1,8 +1,18 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./loginPage.css";
 
 const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await axios.post("/api/login", { email, password });
+    console.log(response);
+  };
+
   return (
     <main className="login__main">
       <div className="login__container">
@@ -10,12 +20,25 @@ const LoginPage = () => {
         <p className="header__subtext">
           We're happy to see you're catching up!
         </p>
-        <form action="/login" method="post">
+        <form onSubmit={handleSubmit}>
           <h2 className="login__label">email</h2>
-          <input className="login__input" type="email" name="email" required />
+          <input
+            className="login__input"
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            type="email"
+            name="email"
+            required
+          />
           <h2 className="login__label">password</h2>
           <input
             className="login__input"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
             type="password"
             name="password"
             required
