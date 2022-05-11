a1bcc8e6ad82f6f8354770159b916e28f7ab4371
Kevin Schwarz <kschwarz99@gmail.com>
2022-05-11 17:57:24 -0400

    edit profile page stlying final

--- client/components/generic/EditProfile/EditProfile.jsx
+++ client/components/generic/EditProfile/EditProfile.jsx
@@ -1,19 +1,61 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import SubmitButton from "../SubmitButton/SubmitButton";
 import "./editProfile.css";
 
 const EditProfile = () => {
   return (
     <div className="edit__profile__container">
-      <h3>Profile Picture</h3>
+      <div className="pfp__edit">
+        <h3 className="edit_subtitles">Profile Picture</h3>
+        <img
+          src="../images/pfp.jpg"
+          alt="placeholder"
+          className="pfp__preview"
+        />
+        <form className="pfp__file__upload" action="">
+          <label for="pfp" className="pfp__btn">
+            Choose File
+          </label>
+          <input
+            type="file"
+            id="pfp"
+            name="filename"
+            accept=".png,.jpg,.jpeg"
+          />
+          <SubmitButton>
+            <input type="submit" />
+          </SubmitButton>
+        </form>
+      </div>
       <hr className="edit__spacer__line" />
-      <h3>First Name*</h3>
+      <div className="edit__content">
+        <h3 className="edit_subtitles">First Name*</h3>
+        <div className="edit__content__container">
+          <p className="user__content">USER CONTENT</p>
+        </div>
+      </div>
       <hr className="edit__spacer__line" />
-      <h3>Last Name*</h3>
+      <div className="edit__content">
+        <h3 className="edit_subtitles">Last Name*</h3>
+        <div className="edit__content__container">
+          <p className="user__content">USER CONTENT</p>
+        </div>
+      </div>
       <hr className="edit__spacer__line" />
-      <h3>Email Address*</h3>
+      <div className="edit__content">
+        <h3 className="edit_subtitles">Email Address*</h3>
+        <div className="edit__content__container">
+          <p className="user__content">USER CONTENT</p>
+        </div>
+      </div>
       <hr className="edit__spacer__line" />
-      <h3>Password</h3>
+      <div className="edit__content">
+        <h3 className="edit_subtitles">Password</h3>
+        <div className="edit__content__container">
+          <p className="user__content">USER CONTENT</p>
+        </div>
+      </div>
       <hr className="edit__spacer__line" />
       <button className="change__password__btn">Change Password</button>
     </div>
